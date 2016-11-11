6daa753b5bcec0e28117c2e5affaedd95b07b881
chenyvehtung <chenyvehtung@163.com>
2016-11-11 17:01:46 +0800

    Finish spatial filter

--- 02_Histogram_and_Spatial_Filtering/src/spatial_filter.py
+++ 02_Histogram_and_Spatial_Filtering/src/spatial_filter.py
@@ -2,11 +2,12 @@
 # -*- coding:utf-8 -*-
 import numpy as np
 from PIL import Image
+from hist_equa import equalize_hist
 
 
 def filter2d(input_img, filter):
-    output_img = np.zeros_like(input_img)
     height, width = input_img.shape
+    output_img = np.zeros((height, width))
     # rotate the filter by 180 degree
     filter_rt = np.rot90(filter, 2)
     # add zero-padding
@@ -20,9 +21,23 @@ def filter2d(input_img, filter):
     for row in xrange(height):
         for col in xrange(width):
             img_patch = pad_img[row:row + filter_len, col:col + filter_len]
-            output_img[row][col] = int(np.sum(np.multiply(filter_rt, img_patch)))
+            output_img[row][col] = np.sum(np.multiply(filter_rt, img_patch))
+
+    # output_img = stand_img(output_img)
 
     return output_img
+
+
+def stand_img(img):
+    """
+    scale output image for display purpose and
+    return the scaled image
+    """
+    min_v = np.amin(img)
+    img -= min_v
+    max_v = np.amax(img)
+    output_img = 255.0 / max_v * img
+    return output_img.astype(np.uint8)
 
 
 def gen_avg_filter(filter_len):
@@ -36,10 +51,30 @@ def main():
     for filter_len in [3, 7, 11]:
         avg_filter = gen_avg_filter(filter_len)
         output_img = filter2d(input_img, avg_filter)
-        output_img = Image.fromarray(output_img, 'L')
+        output_img = Image.fromarray(output_img.astype(np.uint8), 'L')
         img_title = "images/avg_filter_%d_%d_72.png" % (filter_len, filter_len)
         output_img.save(img_title)
         print "Successfully saved %s" % img_title
+
+    # sharpen image using 3 × 3 Laplacian filter
+    lap_filter = np.array([0, 1, 0, 1, -4, 1, 0, 1, 0]).reshape((3, 3))
+    output_img = filter2d(input_img, lap_filter)
+    output_img = stand_img(output_img)
+    enhance_img = stand_img(input_img.astype(np.int64) - output_img)
+    enhance_img = Image.fromarray(enhance_img, 'L')
+    img_title = "images/sharpen_72.png"
+    enhance_img.save(img_title)
+    print "Successfully saved %s" % img_title
+
+    # high boost filtering
+    blur_img = filter2d(input_img, gen_avg_filter(7))
+    g_mask = stand_img(input_img - blur_img)
+    k = 1.3
+    g_output = stand_img(input_img.astype(np.int64) + k * g_mask.astype(np.int64))
+    output_img = Image.fromarray(g_output, 'L')
+    img_title = "images/hboost_%.1f_72.png" % k
+    output_img.save(img_title)
+    print "Successfully saved %s" % img_title
 
 
 if __name__ == "__main__":
