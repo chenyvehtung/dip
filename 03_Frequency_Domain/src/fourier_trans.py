fa9bd967c440e3711ef438ada48a1a31c3430c62
chenyvehtung <chenyvehtung@163.com>
2016-11-21 00:24:45 +0800

    use itertools to parallel nested for loop

--- 03_Frequency_Domain/src/fourier_trans.py
+++ 03_Frequency_Domain/src/fourier_trans.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 import numpy as np
 from PIL import Image
+import itertools
+import time
 
 
 def dft2d(input_img, flags):
@@ -27,14 +29,22 @@ def dft2d(input_img, flags):
         for v in xrange(N):
             e_v.append(np.exp(-1j * 2 * np.pi * v / N * np.arange(N).reshape(1, N)))
 
-        for u in xrange(M):
-            for v in xrange(N):
-                e_part = np.multiply(e_u[u], e_v[v])
-                output_img[u, v] = np.mean(np.multiply(input_img[x, y], e_part))
+        for u, v in itertools.product(xrange(M), xrange(N)):
+            # print u, v
+            e_part = np.multiply(e_u[u], e_v[v])
+            output_img[u, v] = np.mean(np.multiply(input_img, e_part))
 
     # Inverse Discrete Fourier Transform (IDFT)
     else:
-        pass
+        for u in xrange(M):
+            for v in xrange(N):
+                s = 0.0+0.j
+                for x in xrange(M):
+                    for y in xrange(N):
+                        s += ((input_img[x, y] + 0.j) * np.exp(-1j * 2 * np.pi *
+                                                              (float(u * x) / M +
+                                                               float(v * y) / N)))
+                output_img[u, v] = s / (M * N)
 
     return output_img
 
@@ -52,12 +62,20 @@ def stand_img(img):
 
 
 def main():
-    # input_img = np.array(Image.open('images/72.png').convert('L'))
-    input_img = np.array([0.4, 0.6, 0.8, 0.2]).reshape((2, 2))
+    input_img = np.array(Image.open('images/72.png').convert('L'))
+    # input_img = np.array([0.4, 0.6, 0.8, 0.2]).reshape((2, 2))
+
+    start_t = time.time()
     output_img = dft2d(input_img, "dft")
+    end_t = time.time()
+    print (end_t - start_t)
     print output_img
-    # output_img = dft2d(input_img, "flags")
-    # print output_img
+
+    start_t = time.time()
+    output_img = dft2d(input_img, "flags")
+    end_t = time.time()
+    print (end_t - start_t)
+    print output_img
 
 
 if __name__ == "__main__":
