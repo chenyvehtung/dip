f17eeceb52be9ff3b2ebeee09b26b08d950cad4d
chenyvehtung <chenyvehtung@163.com>
2016-11-21 21:13:28 +0800

    try multiprocessing for dft and finish dft

---
+++ 03_Frequency_Domain/report.md
@@ -0,0 +1,12 @@
+# HW3: Filtering in the Frequency Domain
+
+
+
+## 2 Programming Tasks
+
+### 2.2 Fourier Transform
+
+* **Perform DFT**
+
+![](src/images/dft_72.png)<br>
+I did try all of my best to improve the Fourier Transformation, but it still take times, the Spectrum above takes about 100s in my machine to generate.
--- 03_Frequency_Domain/src/fourier_trans.py
+++ 03_Frequency_Domain/src/fourier_trans.py
@@ -4,12 +4,14 @@ import numpy as np
 from PIL import Image
 import itertools
 import time
+import os.path
 
 
 def dft2d(input_img, flags):
     M, N = input_img.shape
     output_img = np.zeros((M, N))
 
+    # Discrete Fourier Transform implement in vector form of numpy
     # for u in xrange(M):
     #     for v in xrange(N):
     #         s = 0.0+0.j
@@ -19,8 +21,6 @@ def dft2d(input_img, flags):
     #                                                       (float(u * x) / M +
     #                                                        float(v * y) / N)))
     #         output_img[u, v] = s / (M * N)
-
-    # Discrete Fourier Transform implement in vector form of numpy
     if flags == "dft":
         e_u = []
         e_v = []
@@ -36,16 +36,51 @@ def dft2d(input_img, flags):
 
     # Inverse Discrete Fourier Transform (IDFT)
     else:
-        for u in xrange(M):
-            for v in xrange(N):
-                s = 0.0+0.j
-                for x in xrange(M):
-                    for y in xrange(N):
-                        s += ((input_img[x, y] + 0.j) * np.exp(-1j * 2 * np.pi *
-                                                              (float(u * x) / M +
-                                                               float(v * y) / N)))
-                output_img[u, v] = s / (M * N)
+        pass
 
+    return output_img
+
+
+# -----------------------------------------------------------------------------
+# I trying to improve Fourier Transform by using multiprocessing in the following
+# code, it works but just a few seconds, so I choose to comment it and use the
+# origin code above
+# def worker(u, v):
+#     e_part = np.multiply(e_u[u], e_v[v])
+#     res = np.mean(np.multiply(input_img, e_part))
+#     return u, v, res
+# def worker_star(u_v):
+#     """convert f([u, v]) to f(u, v)"""
+#     return worker(*u_v)
+# def mul_dft2d(img):
+#     import multiprocessing
+#     global e_u
+#     global e_v
+#     global input_img
+#     input_img = img
+#     M, N = input_img.shape
+#     e_u = []
+#     e_v = []
+#     for u in xrange(M):
+#         e_u.append(np.exp(-1j * 2 * np.pi * u / M * np.arange(M).reshape(M, 1)))
+#     for v in xrange(N):
+#         e_v.append(np.exp(-1j * 2 * np.pi * v / N * np.arange(N).reshape(1, N)))
+#     output_img = np.zeros((M, N))
+#     pool = multiprocessing.Pool(processes=8)
+#     for u, v, val in pool.map(worker_star, itertools.product(xrange(M), xrange(N))):
+#         output_img[u, v] = val
+#     return output_img
+# -----------------------------------------------------------------------------
+
+
+def shift_img(input_img):
+    """
+    Shift image to centralize the Fourier Spectrum
+    """
+    M, N = input_img.shape
+    output_img = np.zeros((M, N))
+    for x, y in itertools.product(xrange(M), xrange(N)):
+        output_img[x, y] = input_img[x, y] * np.power(-1, x + y)
     return output_img
 
 
@@ -64,18 +99,23 @@ def stand_img(img):
 def main():
     input_img = np.array(Image.open('images/72.png').convert('L'))
     # input_img = np.array([0.4, 0.6, 0.8, 0.2]).reshape((2, 2))
+    shifted_img = shift_img(input_img)
 
-    start_t = time.time()
-    output_img = dft2d(input_img, "dft")
-    end_t = time.time()
-    print (end_t - start_t)
-    print output_img
+    dft_path = 'dft.npy'
+    if os.path.isfile(dft_path):
+        output_img = np.load(dft_path)
+    else:
+        start_t = time.time()
+        output_img = dft2d(shifted_img, "dft")
+        end_t = time.time()
+        print (end_t - start_t)
+        np.save(dft_path, output_img)
 
-    start_t = time.time()
-    output_img = dft2d(input_img, "flags")
-    end_t = time.time()
-    print (end_t - start_t)
-    print output_img
+    output_img = stand_img(output_img)
+    output_img = Image.fromarray(output_img, 'L')
+    img_title = "images/dft_72.png"
+    output_img.save(img_title)
+    print "Successfully saved ", img_title
 
 
 if __name__ == "__main__":
