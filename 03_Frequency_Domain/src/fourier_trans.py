3233ba6be0e3c96418cf2558786336a936db4f26
chenyvehtung <chenyvehtung@163.com>
2016-11-24 21:38:47 +0800

    Fix error of dft and idft

--- 03_Frequency_Domain/src/fourier_trans.py
+++ 03_Frequency_Domain/src/fourier_trans.py
@@ -4,80 +4,46 @@ import numpy as np
 from PIL import Image
 import itertools
 import time
-import os.path
+import multiprocessing
 
 
-def dft2d(input_img, flags):
+def naive_dft2d(input_img, flags='dft'):
     M, N = input_img.shape
     output_img = np.zeros((M, N)) + 0.j
-
-    # Discrete Fourier Transform implement in vector form of numpy
-    # for u in xrange(M):
-    #     for v in xrange(N):
-    #         s = 0.0+0.j
-    #         for x in xrange(M):
-    #             for y in xrange(N):
-    #                 s += ((input_img[x, y] + 0.j) * np.exp(-1j * 2 * np.pi *
-    #                                                       (float(u * x) / M +
-    #                                                        float(v * y) / N)))
-    #         output_img[u, v] = s / (M * N)
-    e_u = []
-    e_v = []
-    if flags == "dft":
-        for u in xrange(M):
-            e_u.append(np.exp(-1j * 2 * np.pi * u / M * np.arange(M).reshape(M, 1)))
+    for u in xrange(M):
         for v in xrange(N):
-            e_v.append(np.exp(-1j * 2 * np.pi * v / N * np.arange(N).reshape(1, N)))
-
-        for u, v in itertools.product(xrange(M), xrange(N)):
-            # print u, v
-            e_part = np.multiply(e_u[u], e_v[v])
-            output_img[u, v] = np.mean(np.multiply(input_img, e_part))
-
-    # Inverse Discrete Fourier Transform (IDFT)
-    else:
-        for u in xrange(M):
-            e_u.append(np.exp(1j * 2 * np.pi * u / M * np.arange(M).reshape(M, 1)))
-        for v in xrange(N):
-            e_v.append(np.exp(1j * 2 * np.pi * v / N * np.arange(N).reshape(1, N)))
-
-        for x, y in itertools.product(xrange(M), xrange(N)):
-            e_part = np.multiply(e_u[x], e_v[y])
-            output_img[x, y] = np.sum(np.multiply(input_img, e_part))
-
+            s = 0.0+0.j
+            for x in xrange(M):
+                for y in xrange(N):
+                    s += ((input_img[x, y] + 0.j) * np.exp(-1j * 2 * np.pi *
+                                                          (float(u * x) / M +
+                                                           float(v * y) / N)))
+            output_img[u, v] = s / (M * N)
     return output_img
 
 
-# -----------------------------------------------------------------------------
-# I trying to improve Fourier Transform by using multiprocessing in the following
-# code, it works but just a few seconds, so I choose to comment it and use the
-# origin code above
-# def worker(u, v):
-#     e_part = np.multiply(e_u[u], e_v[v])
-#     res = np.mean(np.multiply(input_img, e_part))
-#     return u, v, res
-# def worker_star(u_v):
-#     """convert f([u, v]) to f(u, v)"""
-#     return worker(*u_v)
-# def mul_dft2d(img):
-#     import multiprocessing
-#     global e_u
-#     global e_v
-#     global input_img
-#     input_img = img
-#     M, N = input_img.shape
-#     e_u = []
-#     e_v = []
-#     for u in xrange(M):
-#         e_u.append(np.exp(-1j * 2 * np.pi * u / M * np.arange(M).reshape(M, 1)))
-#     for v in xrange(N):
-#         e_v.append(np.exp(-1j * 2 * np.pi * v / N * np.arange(N).reshape(1, N)))
-#     output_img = np.zeros((M, N))
-#     pool = multiprocessing.Pool(processes=8)
-#     for u, v, val in pool.map(worker_star, itertools.product(xrange(M), xrange(N))):
-#         output_img[u, v] = val
-#     return output_img
-# -----------------------------------------------------------------------------
+def dft1d(fx):
+    M = fx.shape[0]
+    u_arr = np.arange(M).reshape((M, 1))
+    x_arr = np.arange(M).reshape((1, M))
+    factor = np.exp(-2j * np.pi * u_arr * x_arr / M)
+    return np.dot(factor, fx)
+
+
+def dft2d(input_img, flags):
+    if flags == 'idft':
+        input_img = np.conjugate(input_img)
+    x_num, y_num =  input_img.shape
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    # 1d dft on row
+    dft_xy = np.array(pool.map(dft1d,  [input_img[xidx, :] for xidx in xrange(x_num)]))
+    # 1d dft on column
+    dft_xy = np.array(pool.map(dft1d, [dft_xy[:, yidx] for yidx in xrange(y_num)]))
+    output_img = np.transpose(dft_xy)
+    if flags == 'dft':
+        output_img /= float(x_num * y_num)
+
+    return output_img
 
 
 def shift_img(input_img):
@@ -104,36 +70,35 @@ def stand_img(img):
     return output_img.astype(np.uint8)
 
 
+def test_dft():
+    a = np.arange(2000.0).reshape((40, 50))
+
+    dft_output = dft2d(a, "dft")
+    print "Is DFT right? ", np.allclose(dft_output, np.fft.fft2(a)/2000.0)
+
+    idft_output = dft2d(dft_output, "idft")
+    print "Is IDFT right? ", np.allclose(idft_output, np.fft.ifft2(dft_output)*2000.0)
+
+
 def main():
     input_img = np.array(Image.open('images/72.png').convert('L'), dtype=np.float64)
 
     # dft
     shifted_img = shift_img(input_img)
-    # npy file is used to accelerate the next try.
-    dft_path = 'dft.npy'
-    if os.path.isfile(dft_path):
-        output_img = np.load(dft_path)
-    else:
-        start_t = time.time()
-        output_img = dft2d(shifted_img, "dft")
-        end_t = time.time()
-        print (end_t - start_t)
-        np.save(dft_path, output_img)
+    start_t = time.time()
+    output_img = dft2d(shifted_img, "dft")
+    end_t = time.time()
+    print "DFT: takes %f seconds" % (end_t - start_t)
     dft_img = Image.fromarray(stand_img(output_img.real), 'L')
     img_title = "images/dft_72.png"
     dft_img.save(img_title)
     print "Successfully saved ", img_title
 
     # idft
-    idft_path = 'idft.npy'
-    if os.path.isfile(idft_path):
-        output_img_2 = np.load(idft_path)
-    else:
-        start_t = time.time()
-        output_img_2 = dft2d(output_img, 'idft')
-        end_t = time.time()
-        print (end_t - start_t)
-        np.save(idft_path, output_img_2)
+    start_t = time.time()
+    output_img_2 = dft2d(output_img, 'idft')
+    end_t = time.time()
+    print "IDFT: takes %f seconds" % (end_t - start_t)
     # get the real part and shift back
     output_img_2 = shift_img(output_img_2.real)
     idft_img = Image.fromarray(output_img_2.astype(np.uint8), "L")
@@ -143,4 +108,5 @@ def main():
 
 
 if __name__ == "__main__":
+    test_dft()
     main()
